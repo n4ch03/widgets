c873000ede6cdaf5c10a406dcf21cf4b35d31a39
Ignacio Esmite <iesmite@gmail.com>
2013-09-24 01:34:47 -0300

    chapter 8

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , map = require('./maproutecontroller')
-  , path = require('path');
+    , routes = require('./routes')
+    , map = require('./maproutecontroller')
+    , http = require('http')
+    , stylus = require('stylus')
+    , app = express();
 
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.directory(__dirname + '/public'));
-app.use(function(req, res, next){
-    throw new Error(req.url + ' not found');
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.staticCache({maxObjects: 100, maxLength: 512}));
+    app.use(stylus.middleware({
+        src: __dirname + '/views'
+        , dest: __dirname + '/public'
+    }));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.directory(__dirname + '/public'));
+    app.use(function(req, res, next){
+        throw new Error(req.url + ' not found');
+    });
+    app.use(function(err, req, res, next) {
+        console.log(err);
+        res.send(err.message);
+    });
 });
-app.use(function(err, req, res, next) {
-    console.log(err);
-    res.send(err.message);
-});
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.configure('development', function(){
+    app.use(express.errorHandler());
+});
 
 app.get('/', routes.index);
+
 var prefixes = ['widgets'];
 
+// map route to controller
 prefixes.forEach(function(prefix) {
     map.mapRoute(app, prefix);
 });
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+
+http.createServer(app).listen(3000);
+
+console.log("Express server listening on port 3000");
--- controllers/widgets.js
+++ controllers/widgets.js
@@ -1,29 +1,39 @@
 var widgets = [
     { id : 1,
         name : "The Great Widget",
-        price : 1000.00
+        price : 1000.00,
+        desc: "A widget of great value"
     }
 ]
 
 // index listing of widgets at /widgets/
 exports.index = function(req, res) {
-    res.send(widgets);
+    res.render('widgets/index', {title : 'Widgets', widgets : widgets});
 };
 
 // display new widget form
 exports.new = function(req, res) {
-    res.send('displaying new widget form');
+    console.log(req.url);
+    var filePath = require('path').normalize(__dirname + "/../public/widgets/new.html");
+    res.sendfile(filePath);
 };
 
 // add a widget
 exports.create = function(req, res) {
+
+    // generate widget id
     var indx = widgets.length + 1;
+
+    // add widget
     widgets[widgets.length] =
     { id : indx,
         name : req.body.widgetname,
-        price : parseFloat(req.body.widgetprice) };
+        price : parseFloat(req.body.widgetprice),
+        desc : req.body.widgetdesc };
+
+    //print out to console and confirm addition to user
     console.log(widgets[indx-1]);
-    res.send('Widget ' + req.body.widgetname + ' added with id ' + indx);
+    res.render('widgets/added', {title: 'Widget Added', widget : widgets[indx-1]});
 };
 
 // show a widget
@@ -32,7 +42,7 @@ exports.show = function(req, res) {
     if (!widgets[indx])
         res.send('There is no widget with id of ' + req.params.id);
     else
-        res.send(widgets[indx]);
+        res.render('widgets/show', {title : 'Show Widget', widget : widgets[indx]});
 };
 
 // delete a widget
@@ -46,17 +56,18 @@ exports.destroy = function(req,res) {
 
 // display edit form
 exports.edit = function(req, res) {
-    res.send('displaying edit form');
+    var indx = parseInt(req.params.id) - 1;
+    res.render('widgets/edit', {title : 'Edit Widget', widget : widgets[indx]});
 };
-
 
 // update a widget
 exports.update = function(req,res) {
     var indx = parseInt(req.params.id) - 1;
     widgets[indx] =
-    { id : indx,
+    { id : indx + 1,
         name : req.body.widgetname,
-        price : parseFloat(req.body.widgetprice)}
+        price : parseFloat(req.body.widgetprice),
+        desc : req.body.widgetdesc}
     console.log(widgets[indx]);
-    res.send ('Updated ' + req.params.id);
+    res.render('widgets/added', {title: 'Widget Edited', widget : widgets[indx]})
 };
